6b9fe7941c9a8997801f1e79ae255fba7c6cbdf4
om salunke <omsaunke343@gmail.com>
2025-10-03 10:50:04 +0530

    api updated

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -49,7 +49,7 @@ const Customizer = () => {
 
     try {
       setGeneratingImg(true);
-      const response = await fetch("http://localhost:8080/api/v1/dalle", {
+      const response = await fetch("https://ai-powered-3d-shopping-website-backend.onrender.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
